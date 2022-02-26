214963e22ebc8dfba1960b2f834446d0e3a5d0ab
RGaldamez <ricardo.j.galdamez@gmail.com@>
2022-02-25 20:26:03 -0600

    Creating initial tests

--- src/test/java/tests/Setup.java
+++ src/test/java/tests/Setup.java
@@ -1,39 +1,100 @@
 package tests;
 
+
 import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 
 
+
 public class Setup {
-
 	static AppiumDriver<MobileElement> driver;
+	 
 	
-	public static void main(String [] args) {
-
-		try {
-			setCapabilititesAndStart();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+	
+	@BeforeTest
+	public void setup() throws Exception {
 		
-	}
-	
-	public static void setCapabilititesAndStart() throws Exception {
+		Reporter.log("Setting up capabilities...",true);
 		DesiredCapabilities capabilitites = new DesiredCapabilities() ;
-//		capabilitites.setCapability("","" );
 		capabilitites.setCapability("deviceName","Pixel 2" );
 		capabilitites.setCapability("udid","emulator-5554" );
 		capabilitites.setCapability("platformName","Android" );
 		capabilitites.setCapability("platformVersion","11" );
 		capabilitites.setCapability("appPackage","com.google.android.deskclock" );
 		capabilitites.setCapability("appActivity","com.android.deskclock.DeskClock" );
-		URL url = new URL("http://127.0.0.1:4723/wd/hub"); 
+		URL appiumServerURL = new URL("http://127.0.0.1:4723/wd/hub"); 
 		
-		driver = new AppiumDriver<MobileElement>(url,capabilitites);
+		driver = new AppiumDriver<MobileElement>(appiumServerURL,capabilitites);
+		Reporter.log("Starting tests",true);
+		
+	}
+	
+	@Test
+	public void alarmClick() {
 		MobileElement alarmElement = driver.findElementByXPath("//rk[@content-desc=\"Alarm\"]/android.widget.TextView");
+		String expectedString = "8:30 AM";
 		alarmElement.click();
+
+		WebDriverWait wait = new WebDriverWait(driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//android.view.ViewGroup[@content-desc=\" Alarm\"])[1]"))); 
+
+
+		MobileElement alarmDescription=null;
+		alarmDescription  = driver.findElementByXPath("//android.widget.TextView[@content-desc=\"8:30 AM\"]");
+		
+		String alarmTime="";
+		if(alarmDescription!=null) {
+			alarmTime = alarmDescription.getText();
+		}
+		
+		Reporter.log("Alarm Time: "+ alarmTime,true);
+		Assert.assertEquals( alarmTime,expectedString);
+		
+		alarmDescription.click();
+		
+	}
+	
+	@Test
+	public void changeAlarmTime() {
+		MobileElement hour9 = driver.findElement(By.xpath("//android.widget.RadialTimePickerView.RadialPickerTouchHelper[@content-desc=\"9\"]"));
+		hour9.click();
+		MobileElement minute25 = driver.findElement(By.xpath("//android.widget.RadialTimePickerView.RadialPickerTouchHelper[@content-desc=\"25\"]"));
+		minute25.click();
+		MobileElement btnOK = driver.findElement(By.id("android:id/button1"));
+		btnOK.click();
+		
+		MobileElement alarmDescription=null;
+		alarmDescription  = driver.findElementByXPath("//android.widget.TextView[@content-desc=\"9:25 AM\"]");
+//		Assert.assertNotNull(alarmDescription.getText(),"9:25 AM");
+		WebDriverWait wait = new WebDriverWait(driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.view.ViewGroup[@content-desc=\"9:25 AM Alarm\"]/android.widget.Switch")));
+		alarmDescription  = driver.findElementByXPath("//android.widget.TextView[@content-desc=\"9:25 AM\"]");
+		
+		MobileElement alarmSwitch = driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"9:25 AM Alarm\"]/android.widget.Switch");
+		
+		Assert.assertEquals(alarmSwitch.getAttribute("checked"),"true");
+	}
+	
+	@AfterTest
+	public  void terminate () {
+		Reporter.log("Cleaning up",true);
+		if (driver != null) {
+			driver.quit();	
+		}
 		
 	}
 	
